98c9917b635fa5304c27fb40109f9a792e759410
alvakleiner <alvakleiner@gmail.com>
2026-02-24 15:31:56 +0100

    feat: Create Transaction class (abstract)

---
+++ src/main/java/edu/ntnu/idatt2003/millions/model/Transaction.java
@@ -0,0 +1,32 @@
+package edu.ntnu.idatt2003.millions.model;
+
+public abstract class Transaction {
+    private Share share;
+    private int week;
+    private TransactionCalculator calculator;
+    private boolean committed;
+
+    protected Transaction(Share share, int week, TransactionCalculator calculator) {
+        this.share = share;
+        this.week = week;
+        this.calculator = calculator;
+    }
+
+    public Share getShare() {
+        return share;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public TransactionCalculator getCalculator() {
+        return calculator;
+    }
+
+    public boolean isCommitted() {
+        return committed;
+    }
+
+    public abstract void commit(Player player);
+}
