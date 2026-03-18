b4389c299d6e11c04865c6bb877ef5324c8e7f31
daralang <daralangved@gmail.com>
2026-03-18 18:10:00 +0100

    feat: create PlayerStatusLevel based on net worth and activity

---
+++ src/main/java/edu/ntnu/idatt2003/millions/model/PlayerStatusLevel.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idatt2003.millions.model;
+
+/**
+ * Represents the progression level of a player in the game.
+ * The status is determined by the players net worth growth and trading activity.
+ * They will be given these levels:
+ * <ul>
+ *     <li>Novice: basic start level</li>
+ *     <li>Investor: players who have been trading for minimum 10 weeks and increased their
+ *          net worth with at least 20%.</li>
+ *     <li> Speculator: players who have been trading for minimum 20 weeks and have
+ *          minimum doubled their net worth.</li>
+ * </ul>
+ */
+public enum PlayerStatusLevel {
+    NOVICE,
+    INVESTOR,
+    SPECULATOR
+}
