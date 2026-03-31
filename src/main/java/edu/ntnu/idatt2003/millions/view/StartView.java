5ebb5e759b2c42bc110fcba0188b80a3cf348355
daralang <daralangved@gmail.com>
2026-03-31 19:43:29 +0200

    feat: add StartView and StartController

---
+++ src/main/java/edu/ntnu/idatt2003/millions/view/StartView.java
@@ -0,0 +1,51 @@
+package edu.ntnu.idatt2003.millions.view;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+
+public class StartView {
+
+    private final Scene scene;
+    private final TextField nameField;
+    private final TextField capitalField;
+    private final Button startButton;
+
+    public StartView() {
+        Text title = new Text("Millions");
+
+        nameField = new TextField();
+        capitalField = new TextField();
+        startButton = new Button("START SPILLET");
+
+        VBox root = new VBox(20,
+                new Label("Navn:"), nameField,
+                new Label("Startkapital:"), capitalField,
+                startButton
+        );
+        root.setAlignment(Pos.CENTER);
+
+        scene = new Scene(root, 900, 700);
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public String getName() {
+        return nameField.getText().trim();
+    }
+
+    public String getCapital() {
+        return capitalField.getText().trim();
+    }
+
+    public Button getStartButton() {
+        return startButton;
+    }
+}
