0b8837d6c678ae78398212611a89e27fd6b9f204
alvakleiner <alvakleiner@gmail.com>
2026-03-31 21:11:01 +0200

    feat: add GameManager skeleton

--- src/main/java/edu/ntnu/idatt2003/millions/manager/GameManager.java
+++ src/main/java/edu/ntnu/idatt2003/millions/manager/GameManager.java
@@ -1,4 +1,24 @@
 package edu.ntnu.idatt2003.millions.manager;
 
+import edu.ntnu.idatt2003.millions.model.Exchange;
+import edu.ntnu.idatt2003.millions.model.Player;
+import java.io.File;
+
+
 public class GameManager {
+
+    private Player player;
+    private Exchange exchange;
+
+    public void saveGame(File file) {
+        // TODO: delegere til JsonGameFileHandler
+    }
+
+    public void createNewGame(String name, String capital, File stockFile) {
+        // TODO: implementeres senere
+    }
+
+    public void loadGame(File file) {
+        // TODO: implementeres senere
+    }
 }
