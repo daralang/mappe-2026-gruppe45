54f9b8848b16ea8e673a5fa05bff6479ea088e17
alvakleiner <alvakleiner@gmail.com>
2026-03-31 21:14:16 +0200

    feat: add interface for game file handlers

---
+++ src/main/java/edu/ntnu/idatt2003/millions/file/GameFileHandler.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idatt2003.millions.file;
+
+import edu.ntnu.idatt2003.millions.model.Exchange;
+import edu.ntnu.idatt2003.millions.model.Player;
+import java.io.File;
+
+/**
+ * Interface for reading and writing game state to and from files.
+ * Implementations may support different file formats, such as JSON.
+ */
+public interface GameFileHandler {
+
+    void saveGame(Player player, Exchange exchange, File file);
+
+    void loadGame(File file);
+}
