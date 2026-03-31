0d0169c69ae59de4190aa2ab2d45e91f0ca3a6cf
daralang <daralangved@gmail.com>
2026-03-31 21:16:14 +0200

    feat: add JsonGameFileHandler skeleton

---
+++ src/main/java/edu/ntnu/idatt2003/millions/file/JsonGameFileHandler.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idatt2003.millions.file;
+
+import java.io.File;
+import edu.ntnu.idatt2003.millions.model.Exchange;
+import edu.ntnu.idatt2003.millions.model.Player;
+
+/**
+ * Handles reading and writing of game state to and from JSON files.
+ */
+public class JsonGameFileHandler implements GameFileHandler {
+
+    public void saveGame(Player player, Exchange exchange, File file) {
+        // TODO: implementeres
+    }
+
+    public void loadGame(File file) {
+        // TODO: implementeres
+    }
+}
