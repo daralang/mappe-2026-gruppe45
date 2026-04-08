256744240ed0be0a7f03a962c95fc755c923c16b
daralang <daralangved@gmail.com>
2026-04-08 17:29:30 +0200

    feat: add StatusCard.java

--- src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/portfolio/PortfolioView.java
+++ src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/portfolio/PortfolioView.java
@@ -3,6 +3,7 @@ package edu.ntnu.idatt2003.millions.view.dashboard.portfolio;
 import edu.ntnu.idatt2003.millions.manager.GameManager;
 import edu.ntnu.idatt2003.millions.view.dashboard.portfolio.card.AvailableFundsCard;
 import edu.ntnu.idatt2003.millions.view.dashboard.portfolio.card.PortfolioValueCard;
+import edu.ntnu.idatt2003.millions.view.dashboard.portfolio.card.StatusCard;
 import edu.ntnu.idatt2003.millions.view.dashboard.portfolio.card.WeeklyChangeCard;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -29,12 +30,14 @@ public class PortfolioView extends HBox {
         WeeklyChangeCard weeklyChangeCard = new WeeklyChangeCard(gameManager);
         AvailableFundsCard availableFundsCard = new AvailableFundsCard(gameManager);
         PortfolioValueCard portfolioValueCard = new PortfolioValueCard(gameManager);
+        StatusCard statusCard = new StatusCard(gameManager);
 
 
         VBox rightCards = new VBox(12,
                 weeklyChangeCard,
                 availableFundsCard,
-                portfolioValueCard
+                portfolioValueCard,
+                statusCard
         );
         rightCards.setMinWidth(220);
         rightCards.setMaxWidth(260);
