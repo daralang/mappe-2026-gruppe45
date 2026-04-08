b5ea7712b2654c736439ce2ddb9e5647d32c658b
alvakleiner <alvakleiner@gmail.com>
2026-04-08 11:29:13 +0200

    feat: add AvailableFundsCard.java

--- src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/portfolio/card/AvailableFundsCard.java
+++ src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/portfolio/card/AvailableFundsCard.java
@@ -1,4 +1,46 @@
 package edu.ntnu.idatt2003.millions.view.dashboard.portfolio.card;
 
-public class AvailableFundsCard {
+import edu.ntnu.idatt2003.millions.manager.GameManager;
+import edu.ntnu.idatt2003.millions.util.CurrencyFormatter;
+import edu.ntnu.idatt2003.millions.util.LanguageManager;
+import edu.ntnu.idatt2003.millions.view.component.Card;
+import javafx.scene.control.Label;
+
+/**
+ * Card displaying the player's currently available funds for trading.
+ */
+public class AvailableFundsCard extends Card {
+
+    private final GameManager gameManager;
+    private final Label valueLabel;
+
+    /**
+     * Constructs a new AvailableFundsCard and initializes the display.
+     *
+     * @param gameManager the game manager containing player and exchange
+     */
+    public AvailableFundsCard(GameManager gameManager) {
+        super(gameManager);
+        this.gameManager = gameManager;
+        setSpacing(4);
+
+        Label titleLabel = new Label(LanguageManager.get("dashboard.availableFunds"));
+        titleLabel.getStyleClass().add("card-label");
+
+        valueLabel = new Label();
+        valueLabel.getStyleClass().add("card-value");
+
+        getChildren().addAll(titleLabel, valueLabel);
+
+        valueLabel.setText(CurrencyFormatter.format(gameManager.getPlayer().getMoney()));
+    }
+
+    /**
+     * Called when the game state has changed.
+     * Refreshes the displayed available funds.
+     */
+    @Override
+    public void onGameUpdated() {
+        valueLabel.setText(CurrencyFormatter.format(gameManager.getPlayer().getMoney()));
+    }
 }
