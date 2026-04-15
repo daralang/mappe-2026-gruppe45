fc01765362f16ba3f27c16db837b7a8a8a5716f2
daralang <daralangved@gmail.com>
2026-04-15 19:55:06 +0200

    feat: bind file browser and game start handlers in StartController

--- src/main/java/edu/ntnu/idatt2003/millions/controller/StartController.java
+++ src/main/java/edu/ntnu/idatt2003/millions/controller/StartController.java
@@ -1,8 +1,11 @@
 package edu.ntnu.idatt2003.millions.controller;
 
 import edu.ntnu.idatt2003.millions.view.StartView;
+import java.util.Objects;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 
 public class StartController {
 
@@ -10,17 +13,67 @@ public class StartController {
     private final StartView view;
 
     public StartController(Stage stage) {
+        Objects.requireNonNull(stage, "Stage cannot be null");
         this.stage = stage;
         this.view = new StartView();
         bindEvents();
     }
 
     private void bindEvents() {
+        view.getBrowseStockFileButton().setOnAction(e -> handleBrowseStockFile());
+        view.getBrowseSaveFileButton().setOnAction(e -> handleBrowseSaveFile());
         view.getStartButton().setOnAction(e -> handleStartGame());
+        view.getLoadButton().setOnAction(e -> handleLoadGame());
+    }
+
+    private void handleBrowseStockFile() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Select stock file");
+        chooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Data files", "*.csv", "*.json"));
+        File file = chooser.showOpenDialog(stage);
+        if (file != null) {
+            view.setStockFilePath(file.getAbsolutePath());
+        }
+    }
+
+    private void handleBrowseSaveFile() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Select save file");
+        chooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Save files", "*.json"));
+        File file = chooser.showOpenDialog(stage);
+        if (file != null) {
+            view.setSaveFilePath(file.getAbsolutePath());
+        }
     }
 
     private void handleStartGame() {
-        // TODO: opprett Player og Exchange, start MainController. Bruk getterne fra StartView.
+        String name = view.getName();
+        String capital = view.getCapital();
+        String stockFilePath = view.getStockFilePath();
+
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Player name cannot be blank");
+        }
+        if (capital.isBlank()) {
+            throw new IllegalArgumentException("Starting capital cannot be blank");
+        }
+        if (stockFilePath.isBlank()) {
+            throw new IllegalArgumentException("Stock file must be selected");
+        }
+
+        // TODO: parse capital, load exchange from file, create Player, start MainController
+    }
+
+    private void handleLoadGame() {
+        String saveFilePath = view.getSaveFilePath();
+
+        if (saveFilePath.isBlank()) {
+            throw new IllegalArgumentException("Save file must be selected");
+        }
+
+        // TODO: load GameState, create MainController from state
     }
 
     public void show() {
@@ -28,4 +81,6 @@ public class StartController {
         stage.setScene(view.getScene());
         stage.show();
     }
+
+
 }
