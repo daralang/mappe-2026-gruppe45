866ad0e621fa472d08b8c5014a0a7025bce4dea4
daralang <daralangved@gmail.com>
2026-05-08 19:13:51 +0200

    feat: add CurrencyConverter interface

---
+++ src/main/java/edu/ntnu/idatt2003/millions/model/currency/CurrencyConverter.java
@@ -0,0 +1,22 @@
+package edu.ntnu.idatt2003.millions.model.currency;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+/**
+ * Converts monetary amounts between currencies.
+ */
+public interface CurrencyConverter {
+
+    /**
+     * Converts the given amount from one currency to another.
+     *
+     * @param amount the amount to convert
+     * @param from   the currency the amount is expressed in
+     * @param to     the currency to convert the amount into
+     * @return the converted amount
+     * @throws NullPointerException     if any argument is null
+     * @throws IllegalArgumentException if either currency is not supported
+     */
+    BigDecimal convert(BigDecimal amount, Currency from, Currency to);
+}
