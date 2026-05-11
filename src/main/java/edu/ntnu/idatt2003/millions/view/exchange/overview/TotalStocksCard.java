9761104b3a2c5027eb16ff86c572189d70f45fbc
daralang <daralangved@gmail.com>
2026-05-11 18:03:07 +0200

    refactor: extract exchange stat cards into WidgetCard subclasses

---
+++ src/main/java/edu/ntnu/idatt2003/millions/view/exchange/overview/TotalStocksCard.java
@@ -0,0 +1,35 @@
+package edu.ntnu.idatt2003.millions.view.exchange.overview;
+
+import edu.ntnu.idatt2003.millions.manager.GameManager;
+import edu.ntnu.idatt2003.millions.view.component.StyledText;
+import edu.ntnu.idatt2003.millions.view.component.WidgetCard;
+
+/**
+ * Widget card displaying the total number of stocks listed on the exchange.
+ * Updates automatically on each week advance via {@link edu.ntnu.idatt2003.millions.observer.GameObserver}.
+ */
+public class TotalStocksCard extends WidgetCard {
+
+    private final GameManager gameManager;
+    private final StyledText valueLabel = StyledText.widgetValue();
+
+    /**
+     * Constructs a new TotalStocksCard.
+     *
+     * @param gameManager the game manager containing the exchange
+     */
+    public TotalStocksCard(GameManager gameManager) {
+        super(gameManager, "exchange.overview.totalStocks");
+        this.gameManager = gameManager;
+        getChildren().addAll(titleLabel, valueLabel);
+        refreshDisplay();
+    }
+
+    /**
+     * Updates the displayed stock count from the exchange.
+     */
+    @Override
+    protected void refreshDisplay() {
+        valueLabel.setText(String.valueOf(gameManager.getExchange().getStocks().size()));
+    }
+}
