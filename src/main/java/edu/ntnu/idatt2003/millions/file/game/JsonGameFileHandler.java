88cc840e140c31eb65cd57dbd1cc93722176085b
daralang <daralangved@gmail.com>
2026-05-12 20:24:49 +0200

    style: design clear sort button

--- src/main/java/edu/ntnu/idatt2003/millions/file/game/JsonGameFileHandler.java
+++ src/main/java/edu/ntnu/idatt2003/millions/file/game/JsonGameFileHandler.java
@@ -35,7 +35,7 @@ public class JsonGameFileHandler implements GameFileHandler {
     /**
      * Constructs a new JsonGameFileHandler.
      * Configures Gson with a custom serializer for transactions
-     * and pretty printing for human-readable output.
+     * and pretty printing for readable output.
      */
     public JsonGameFileHandler() {
         this.gson = new GsonBuilder()
