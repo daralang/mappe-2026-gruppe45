14420c902eeb1570456c2f428b4979ac708c454e
daralang <daralangved@gmail.com>
2026-05-15 01:42:11 +0200

    refactor: filters to be on the same line

--- src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/transactions/card/LoanLedgerCard.java
+++ src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/transactions/card/LoanLedgerCard.java
@@ -18,7 +18,6 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -106,10 +105,8 @@ public class LoanLedgerCard extends SortableTableCard<LoanLedgerEntry, LoanLedge
                 "search.button",
                 searchCallback(),
                 metadataRow);
-        searchBar.setMaxWidth(Double.MAX_VALUE);
-        HBox.setHgrow(searchBar, Priority.ALWAYS);
         HBox row = new HBox(16, searchBar, typeFilter, weekRangeFilter, clearSortButton);
-        row.setAlignment(Pos.CENTER_LEFT);
+        row.setAlignment(Pos.TOP_LEFT);
         row.getStyleClass().add("transactions-filter-row");
         return row;
     }
