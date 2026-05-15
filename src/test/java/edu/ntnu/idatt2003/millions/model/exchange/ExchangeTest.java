90de6146912ce1a1322da37a1611650eb32b6b1e
daralang <daralangved@gmail.com>
2026-05-15 13:10:18 +0200

    refactor: move exchangetest to a own folder

--- src/test/java/edu/ntnu/idatt2003/millions/model/exchange/ExchangeTest.java
+++ src/test/java/edu/ntnu/idatt2003/millions/model/exchange/ExchangeTest.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idatt2003.millions.model;
+package edu.ntnu.idatt2003.millions.model.exchange;
 
 import edu.ntnu.idatt2003.millions.model.currency.CurrencyConverter;
 import edu.ntnu.idatt2003.millions.model.currency.FixedRateCurrencyConverter;
