cb7f51dfc6042adb306540f92c894cc66dd37f12
daralang <daralangved@gmail.com>
2026-05-15 18:26:07 +0200

    docs: update javadoc

--- src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/watchlist/WatchlistView.java
+++ src/main/java/edu/ntnu/idatt2003/millions/view/dashboard/watchlist/WatchlistView.java
@@ -7,11 +7,9 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 /**
- * View wrapping the player's watchlist.
- *
- * <p>Acts as a thin layout container that instantiates {@link WatchlistCard}
- * and lets it fill the available vertical space. Tab navigation and lazy
- * initialization are handled by the parent {@link edu.ntnu.idatt2003.millions.view.dashboard.DashboardView}.</p>
+ * View wrapping the player's watchlist view displayed under the dashboard.
+ * Shows the watchlist, list of stocks that user might want to keep track of,
+ * with table of common stock info and action buttons, buy, note and remove.
  */
 public class WatchlistView extends VBox {
 
@@ -19,15 +17,15 @@ public class WatchlistView extends VBox {
      * Constructs a new WatchlistView.
      *
      * @param gameService     the game service containing player and exchange state
-     * @param tradeController the controller used to open buy dialogs
+     * @param controller      the controller used to open buy dialogs
      * @param onExploreStocks callback invoked when the player clicks the explore button
      */
     public WatchlistView(GameService gameService,
-                         TradeController tradeController,
+                         TradeController controller,
                          Runnable onExploreStocks) {
         setSpacing(16);
 
-        WatchlistCard card = new WatchlistCard(gameService, tradeController, onExploreStocks);
+        WatchlistCard card = new WatchlistCard(gameService, controller, onExploreStocks);
         VBox.setVgrow(card, Priority.ALWAYS);
 
         ExploreStocksButton exploreButton = new ExploreStocksButton(onExploreStocks);
