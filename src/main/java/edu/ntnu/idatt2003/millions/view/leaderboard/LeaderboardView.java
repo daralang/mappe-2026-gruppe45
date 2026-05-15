119682bbc69db0503774ca864ba9ba8dace8b773
daralang <daralangved@gmail.com>
2026-05-15 23:00:47 +0200

    feat: add weekbar to LeaderboardView

--- src/main/java/edu/ntnu/idatt2003/millions/view/leaderboard/LeaderboardView.java
+++ src/main/java/edu/ntnu/idatt2003/millions/view/leaderboard/LeaderboardView.java
@@ -4,7 +4,10 @@ import edu.ntnu.idatt2003.millions.service.GameService;
 import edu.ntnu.idatt2003.millions.service.toast.ToastService;
 import edu.ntnu.idatt2003.millions.util.LanguageManager;
 import edu.ntnu.idatt2003.millions.view.component.StyledText;
+import edu.ntnu.idatt2003.millions.view.component.WeekBar;
 import edu.ntnu.idatt2003.millions.view.leaderboard.card.LeaderboardCard;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
@@ -21,18 +24,24 @@ public class LeaderboardView extends VBox {
     /**
      * Constructs a new LeaderboardView.
      *
-     * @param gameService the game service used by the card for observer registration
+     * @param gameService  the game service used by the card for observer registration
+     * @param toastService the toast service used to display notifications
+     * @param weekBar      the shared week bar component
      */
-    public LeaderboardView(GameService gameService, ToastService toastService) {
+    public LeaderboardView(GameService gameService, ToastService toastService, WeekBar weekBar) {
         getStyleClass().add("content-area");
 
         StyledText pageTitle = StyledText.pageTitle(LanguageManager.get("leaderboard.title"));
         LanguageManager.addObserver(
                 () -> pageTitle.setText(LanguageManager.get("leaderboard.title")));
 
+        HBox titleRow = new HBox(12, pageTitle, weekBar);
+        HBox.setHgrow(weekBar, Priority.ALWAYS);
+        titleRow.setAlignment(Pos.CENTER_LEFT);
+
         LeaderboardCard card = new LeaderboardCard(gameService, toastService);
         VBox.setVgrow(card, Priority.ALWAYS);
 
-        getChildren().addAll(pageTitle, card);
+        getChildren().addAll(titleRow, card);
     }
 }
