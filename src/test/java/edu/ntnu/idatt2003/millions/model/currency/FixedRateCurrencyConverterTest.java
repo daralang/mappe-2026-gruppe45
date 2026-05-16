64fd0576342d8c9d938152b3a316943ecd0356ca
alvakleiner <alvakleiner@gmail.com>
2026-05-16 11:07:23 +0200

    chore: set up test folder structure to mirror main's

--- src/test/java/edu/ntnu/idatt2003/millions/model/currency/FixedRateCurrencyConverterTest.java
+++ src/test/java/edu/ntnu/idatt2003/millions/model/currency/FixedRateCurrencyConverterTest.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idatt2003.millions.model;
+package edu.ntnu.idatt2003.millions.model.currency;
 
 import edu.ntnu.idatt2003.millions.model.currency.FixedRateCurrencyConverter;
 import org.junit.jupiter.api.BeforeEach;
