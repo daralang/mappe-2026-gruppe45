853c7826358d4d697e60ef102a80859a6b15ca1f
daralang <daralangved@gmail.com>
2026-05-16 11:07:23 +0200

    chore: set up test folder structure to mirror main's

--- src/test/java/edu/ntnu/idatt2003/millions/model/calculator/PurchaseCalculatorTest.java
+++ src/test/java/edu/ntnu/idatt2003/millions/model/calculator/PurchaseCalculatorTest.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idatt2003.millions.model;
+package edu.ntnu.idatt2003.millions.model.calculator;
 
 import edu.ntnu.idatt2003.millions.model.calculator.PurchaseCalculator;
 import edu.ntnu.idatt2003.millions.model.stock.Share;
--- src/test/java/edu/ntnu/idatt2003/millions/model/stock/ShareTest.java
+++ src/test/java/edu/ntnu/idatt2003/millions/model/stock/ShareTest.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idatt2003.millions.model;
+package edu.ntnu.idatt2003.millions.model.stock;
 
 import edu.ntnu.idatt2003.millions.model.stock.Share;
 import edu.ntnu.idatt2003.millions.model.stock.Stock;
